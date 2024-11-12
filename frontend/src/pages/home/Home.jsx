2b5d533889f8f8e315326881d8c2a33e91f5f2a3
massiferrouk <massiferrouk3@gmail.com>
2024-11-12 21:36:34 +0100

    Finalized the TopSellers Section

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from './Banner'
+import TopSellers from './TopSellers'
+
+const home = () => {
+  return (
+    <>
+        <Banner />
+        <TopSellers />
+    </>
+  )
+}
+
+export default home
